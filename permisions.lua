2d64a89410683ac20b52d083b28a07ab86394745
powerboat9 <powerboat9@users.noreply.github.com>
2017-02-25 12:29:51 -0500

    Update permisions.lua

--- permisions.lua
+++ permisions.lua
@@ -16,27 +16,76 @@ local function dupeTable(t1)
 end
 
 do
-    local old = _G.fs
+    local old = dupeTable(_G)
     local permTables = {}
+    local function isValidPermission(s)
+        --BIOS.lua disables changing string metatable - more secure than var "old" (still secure)
+        return type(s) == "string" and s:gsub("%.%.+", "") == s and s:gsub("[^A-Za-z0-9_%-%.]") == s
+    end
+    local function normalizePermission(s)
+        if not isValidPermission(s) then
+            old.error("Invalid permission", 2)
+        end
+        if s:sub(1, 1) == "." then
+            return s
+        else
+            return "." .. s
+        end
+    end
     local function dupeKey(k1)
-        if not permTables[k] then
-            error("Invalid key", 2)
+        if not permTables[k1] then
+            old.error("Invalid key", 2)
         else
             local k2 = {}
-            permTables[k2] = dupeTable(permTables[k])
+            permTables[k2] = dupeTable(permTables[k1])
             return k2
         end
     end
     local function getPermission(k, n)
-        for k, v in pairs(permTables[k]) do
-            if st
+        if not permTables[k] then
+            old.error("Invalid key", 2)
+        elseif not isValidPermission(n) then
+            old.error("Invalid permission", 2)
+        end
+        if permTables[k][n] ~= nil then
+            return permTables[k][n], true
+        end
+        n = normalizePermission(n)
+        local lastFN, lastFV = ".", permTables[k]["."] or false
+        for fn, fv in pairs(permTables[k]) do
+            if #lastFN < #fn and #fn < #n and n:sub(1, #fn + 1) == (fn .. ".") then
+                lastFN, lastFV = fn, fv
+            end
+        end
+        return lastFV, false, lastFN
+    end
     local function setPermission(mainK, k, n, v)
         if not permTables[mainK] then
-            error("Invalid main key", 2)
+            old.error("Invalid main key", 2)
         elseif not permTables[k] then
-            error("Invalid key", 2)
-        elseif type(n) ~= "string" then
-            error("Invalid permission", 2)
+            old.error("Invalid key", 2)
+        elseif not isValidPermission(n) then
+            old.error("Invalid permission", 2)
         elseif (v ~= true) and (v ~= false) and (v ~= nil) then
-            error("Invalid permission value", 2)
-        elseif not getPermission()
+            old.error("Invalid permission value", 2)
+        elseif not getPermission(mainK, ".allowSettingPermissions")
+            old.error("Cannot use main key to set permissions", 2)
+        else
+            n = normalizePermission(n)
+            if v then
+                local authorized, wasDirrect = getPermission(mainK, n)
+                if not authorized or not (wasDirrect or getPermission(mainK, ".allowIndirrectPermissionAuthorization")) then
+                    old.error("Unauthorized", 2)
+                end
+            elseif v == nil and permTables[k][n] == false and not getPermission(mainK, ".allowDisablingBlacklist") then
+                old.error("Unauthorized to remove blacklist", 2)
+            end
+            permTables[k][n] = v
+        end
+    end
+    local function getNewEnv()
+        local newEnv = dupeTable(old)
+        newEnv.perm = {
+            getPermission = getPermission,
+            setPermission = setPermission,
+            dupeKey = dupeKey,
